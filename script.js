e11ffc3fefaa98675cf40ea6d5c21a7e9056f53d
Sanjay Patel <worldprotv@gmail.com>
2020-09-10 03:13:06 +0530

    portfolio

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ GitHubCalendar(".calendar", "sanjaypatel29",
         responsive: true
     });
 
+//For Changing the texts inside the changingRoles Div
+let i = 0
+const profession = [ ' Full Stack Developer','Problem Solver','Web Designer', 'Continuos Learner']
+
+setInterval(()=>{
+    if(i >= profession.length){
+        i = 0
+    }
+    changingprofession(i++)
+},1000)
+
+function changingprofession(i){
+    if(i < profession.length){
+        let changingprofessionDiv = document.getElementById('changingprofession')
+        changingprofessionDiv.innerHTML = `<h2>${profession[i]}</h2>`
+        i += 1
+    }
+}
