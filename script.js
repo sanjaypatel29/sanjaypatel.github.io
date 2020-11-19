1290944e76e5b489800c957e8ffd8bc531354421
Sanjay Patel <worldprotv@gmail.com>
2020-11-19 13:53:04 +0530

    ProjectUpdate

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ GitHubCalendar(".calendar", "sanjaypatel29",
 
 
 let i = 0
-const profession = [ ' Full Stack Developer','Problem Solver','Web Designer', 'Continuos Learner']
+const profession = [' Full Stack Developer', 'Problem Solver', 'Web Designer', 'Continuos Learner']
 
-setInterval(()=>{
-    if(i >= profession.length){
+setInterval(() => {
+    if (i >= profession.length) {
         i = 0
     }
     changingprofession(i++)
-},1000)
+}, 1000)
 
-function changingprofession(i){
-    if(i < profession.length){
+function changingprofession(i) {
+    if (i < profession.length) {
         let changingprofessionDiv = document.getElementById('changingprofession')
         changingprofessionDiv.innerHTML = `<h2>${profession[i]}</h2>`
         i += 1
